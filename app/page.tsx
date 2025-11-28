6c40ef30417d5e89aae61def7b6f9ddab8f5756a
kavyakapoor420 <kavyakapoor412@gmail.com>
2025-11-28 23:00:08 +0530

    impotred navbar hero in main home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
+import Bento from './components/landing/Bento'
+import Hero from "./components/landing/Hero";
+import Navbar from "./components/landing/Navbar";
+import Image from "next/image";
 
 
 export default function Home() {
   
-
   return (
     <div className="">
       <main className="">
        
-        
+        <Navbar/>
+        <Hero/>
+        <Bento/>
        
 
        
